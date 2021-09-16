7f70ede5aafc4d402637d1c775969a802c6fab51
MohamedBechir <jamoussi.bechir@yahoo.com>
2021-09-16 21:09:13 +0200

    create send messages to queue page

--- src/app/pages/sendMessagePage/index.tsx
+++ src/app/pages/sendMessagePage/index.tsx
@@ -1,41 +1,32 @@
 /**
- * Send Message Page
+ * Send Messagess Page
  */
 
 import { Footer } from 'app/components/Footer';
 import { CustomNavbar } from 'app/components/Navbar';
-import { useEffect } from 'react';
 import { Button } from 'react-bootstrap';
-import ReactJson from 'react-json-view';
-import { useDispatch, useSelector } from 'react-redux';
-import '../sendMessagePage/design.css';
-import { useFetchJsonMessages } from './slice';
-import { selectState } from './slice/selectors';
 
 export function SendMessagePage() {
-  const { actions } = useFetchJsonMessages();
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(actions.requestFetchJsonMessages());
-  }, [actions, dispatch]);
-  const jsonMessages = useSelector(selectState);
-
   return (
     <>
       <CustomNavbar />
-      <h2 className="ml-5" style={{ color: '#f49d37' }}>
-        MT101: ID1202
-      </h2>
-      <div className="row d-flex w-75 ml-5" style={{ height: '300px' }}>
-        <div>
-          {jsonMessages.jsonMessages.map(jsonMessage => (
-            <>
-              <ReactJson src={jsonMessage} />
-              <Button variant="warning" className="w-25 mt-2 mb-2">
-                Send to Queue
-              </Button>
-            </>
-          ))}
+      <div className="container align-items-center mt-5">
+        <div className="col-md-12 text-center mt-5">
+          <h1>Choose the Type of Message That you will send:</h1>
+          <Button
+            href="/send-message/json"
+            size="lg"
+            className="btn btn-primary mt-5 mr-5"
+          >
+            Send JSON Message
+          </Button>
+          <Button
+            href="/send-message/xml"
+            size="lg"
+            className="btn btn-warning mt-5"
+          >
+            Send XML Message
+          </Button>
         </div>
       </div>
       <Footer />
